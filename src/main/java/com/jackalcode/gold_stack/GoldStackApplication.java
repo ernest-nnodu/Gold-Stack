5687055b466c0e6151faa2c2585765b16461bc07
ernest-nnodu <ernest.nnodu@outlook.com>
2026-05-16 08:53:22 +0100

    Initial setup

---
+++ src/main/java/com/jackalcode/gold_stack/GoldStackApplication.java
@@ -0,0 +1,14 @@
+package com.jackalcode.gold_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldStackApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GoldStackApplication.class, args);
+    }
+
+}
