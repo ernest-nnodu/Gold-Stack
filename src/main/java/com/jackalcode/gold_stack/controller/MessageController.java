68e82ea90e50dda4be0a4fa4dc86827abbdd6b84
ernest-nnodu <ernest.nnodu@outlook.com>
2026-05-17 13:49:20 +0100

    Add get custom message endpoint

--- src/main/java/com/jackalcode/gold_stack/controller/MessageController.java
+++ src/main/java/com/jackalcode/gold_stack/controller/MessageController.java
@@ -4,11 +4,9 @@ import com.jackalcode.gold_stack.entity.MessageEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/messages")
 public class MessageController {
 
     @GetMapping
@@ -18,10 +16,17 @@ public class MessageController {
         return ResponseEntity.ok(message);
     }
 
-    @GetMapping("/error")
+    @GetMapping(path = "/error")
     public ResponseEntity<MessageEntity> getErrorMessage() {
 
         var message = new MessageEntity("This is an error message from Gold Stack in AWS!");
         return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(message);
+    }
+
+    @GetMapping(path = "/custom/**")
+    public ResponseEntity<MessageEntity> getCustomMessage() {
+
+        var message = new MessageEntity("Message not found in Gold Stack in AWS!");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
     }
 }
