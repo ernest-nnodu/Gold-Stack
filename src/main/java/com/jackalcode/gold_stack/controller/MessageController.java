6e79d7a18bd4e9286c01fec1ee1cddc7807a12eb
ernest-nnodu <ernest.nnodu@outlook.com>
2026-05-17 13:00:15 +0100

    Add happy message endpoint

--- src/main/java/com/jackalcode/gold_stack/controller/MessageController.java
+++ src/main/java/com/jackalcode/gold_stack/controller/MessageController.java
@@ -1,5 +1,8 @@
 package com.jackalcode.gold_stack.controller;
 
+import com.jackalcode.gold_stack.entity.MessageEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/messages")
 public class MessageController {
 
+    @GetMapping
+    public ResponseEntity<MessageEntity> getHappyMessage() {
+
+    MessageEntity message = new MessageEntity("Hello From Gold Stack in AWS!");
+        return ResponseEntity.ok(message);
+    }
 }
