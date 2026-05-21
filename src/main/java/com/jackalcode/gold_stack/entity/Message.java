6437cdaa6712781c35a08650561ff8e73557db99
ernest-nnodu <ernest.nnodu@outlook.com>
2026-05-21 21:03:37 +0100

    Add message entity with id, description, content, and createdAt fields

---
+++ src/main/java/com/jackalcode/gold_stack/entity/Message.java
@@ -0,0 +1,51 @@
+package com.jackalcode.gold_stack.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "messages")
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @PrePersist
+    protected void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id) &&
+                Objects.equals(description, message.description) &&
+                Objects.equals(content, message.content) &&
+                Objects.equals(createdAt, message.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, content, createdAt);
+    }
+}
