7e0a521eeba24ad84f63d24ff0747524ee122e9c
ernest-nnodu <ernest.nnodu@outlook.com>
2026-05-24 17:36:23 +0100

    Add API endpoints for message

--- src/main/java/com/jackalcode/gold_stack/controller/MessageController.java
+++ src/main/java/com/jackalcode/gold_stack/controller/MessageController.java
@@ -1,15 +1,42 @@
 package com.jackalcode.gold_stack.controller;
 
+import com.jackalcode.gold_stack.dto.CreateMessageRequest;
 import com.jackalcode.gold_stack.dto.MessageResponse;
+import com.jackalcode.gold_stack.service.MessageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class MessageController {
+
+    private final MessageService messageService;
+
+    @GetMapping(path = "/messages")
+    public ResponseEntity<List<MessageResponse>> getMessages() {
+
+        return ResponseEntity.ok(messageService.getMessages());
+    }
+
+    @GetMapping(path = "/messages/{id}")
+    public ResponseEntity<MessageResponse> getMessage(Long id) {
+
+        return ResponseEntity.ok(messageService.getMessage(id));
+    }
+
+    @PostMapping(path = "/messages")
+    public ResponseEntity<MessageResponse> createMessage(CreateMessageRequest messageRequest) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(messageService.createMessage(messageRequest));
+    }
 
     @GetMapping
     public ResponseEntity<MessageResponse> getHappyMessage() {
